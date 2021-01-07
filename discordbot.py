6891edde84f10b9b9f8721e49e0fec6f3bf4b674
nyuyoku1117 <33619909+nyuyoku1117@users.noreply.github.com>
2021-01-08 00:44:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -126,20 +126,3 @@ while True:
         get_information()
     check_schedule(now_time, broadcast_data)
     time.sleep(60)
-
-
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
